10c700bcde470804c68d7c6cbac3f713d20810f1
raishkhan311 <raishkhan311@gmail.com>
2017-05-15 11:09:18 +0530

    for example demo image picker

--- ImagePicker/ViewController.swift
+++ ImagePicker/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,44 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var img_ImageView: UIImageView!
+    
+    @IBAction func btn_ChooseImage(_ sender: Any) {
+        
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        
+        let actionsheet = UIAlertController(title: "photo source", message: "choose source", preferredStyle: .actionSheet)
+        
+        actionsheet.addAction(UIAlertAction(title: "camera", style: .default, handler:{(action:UIAlertAction) in
+            if UIImagePickerController.isSourceTypeAvailable(.camera){
+                imagePickerController.sourceType = .camera
+                self.present(imagePickerController, animated: true, completion: nil)
+            }else{
+                print("camer is not available")
+            }
+            
+        }))
+        
+        actionsheet.addAction(UIAlertAction(title: "photo Library", style: .default, handler:{(action:UIAlertAction) in
+            imagePickerController.sourceType = .photoLibrary
+            self.present(imagePickerController, animated: true, completion: nil)
+        }))
+        
+        actionsheet.addAction(UIAlertAction(title: "cancel", style: .cancel, handler:nil))
+        
+        self.present(actionsheet, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        img_ImageView.image = image
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
 
 }
 
